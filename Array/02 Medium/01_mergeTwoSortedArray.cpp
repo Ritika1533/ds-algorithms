4d6f00011ea4f27bd95a924e94fb8e9cc3be2a41
unknown <ritikakes33@gmail.com>
2024-06-01 21:52:44 +0530

    dsa

---
+++ Array/02 Medium/01_mergeTwoSortedArray.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+void mergeTwoSortedArray(int *arr1, int *arr2, int *arr3, int size1, int size2)
+{
+    int index1 = 0;
+    int index2 = 0;
+    int index3 = 0;
+    while (index1 < size1 && index2 < size2)
+    {
+        if (arr1[index1] < arr2[index2])
+        {
+            arr3[index3++] = arr1[index1++];
+        }
+        else
+        {
+            arr3[index3++] = arr2[index2++];
+        }
+    }
+    // o(min(size1,size2))
+    while (index1 < size1)
+    {
+        arr3[index3++] = arr1[index1++];
+    }
+    // o(size1-index1) : worst case =>index1=0
+    // o(size1)
+    while (index2 < size2)
+    {
+        arr3[index3++] = arr2[index2++];
+    }
+    // o(size2-index2) : worst case =>index2=0
+    // o(size2)
+}
+// OVERALL TIME COMPLEXITY : O()
+void printArray(int *arr, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+int main()
+{
+    int size1, size2;
+    int arr1[] = {68, 90, 92, 115, 167};
+    int arr2[] = {1, 56, 89, 99, 245};
+    size1 = sizeof(arr1) / sizeof(arr1[0]);
+    size2 = sizeof(arr2) / sizeof(arr2[0]);
+    printArray(arr1, size1);
+    printArray(arr2, size2);
+    int *arr3 = new int[size1 + size2];
+    mergeTwoSortedArray(arr1, arr2, arr3, size1, size2);
+    printArray(arr3, size1 + size2);
+}
+/*68 90 92 115 167
+1 56 89 99 245
+1 56 68 89 90 92 99 115 167 245*/
---
+++ Recursion/4  Sorting/04 Recursive Merge Sort/1_mergeSort.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+using namespace std;
+void merge(int *arr, int s, int e)
+{
+    int mid = s + (e - s) / 2;
+    // finding array length
+    int len1 = mid - s + 1;
+    int len2 = e - mid;
+    // creating new array
+    int *first = new int[len1];
+    int *second = new int[len2];
+    // copying array
+    int k = s;
+    for (int i = 0; i < len1; i++)
+    {
+        first[i] = arr[k++];
+    }
+    k = mid + 1;
+    for (int i = 0; i < len2; i++)
+    {
+        second[i] = arr[k++];
+    }
+    // merge two array
+    int index1 = 0;
+    int index2 = 0;
+    k = s;
+    while (index1 < len1 && index2 < len2)
+    {
+        if (first[index1] < second[index2])
+        {
+            arr[k++] = first[index1++];
+        }
+        else
+        {
+            arr[k++] = second[index2++];
+        }
+    }
+    while (index1 < len1)
+    {
+        arr[k++] = first[index1++];
+    }
+
+    while (index2 < len2)
+    {
+        arr[k++] = second[index2++];
+    }
+}
+void mergeSort(int *arr, int s, int e)
+{
+    if (s >= e)
+    {
+        return;
+    }
+    int mid = s + (e - s) / 2;
+    // left part sort
+    mergeSort(arr, s, mid);
+    // right part sort
+    mergeSort(arr, mid + 1, e);
+    // merge two sorted array
+    merge(arr, s, e);
+}
+void printArray(int *arr, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+int main()
+{
+    int arr[] = {6, 9, 3, 0, 14, 56, 344, 98};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    printArray(arr, size);
+    mergeSort(arr, 0, size - 1);
+    printArray(arr, size);
+}

f86acd3458381d7651ae647880948e485a9a5379
unknown <ritikakes33@gmail.com>
2024-06-05 20:48:10 +0530

    dsa

---
+++ Array/01 Easy/1002_findCommomCharacter.cpp
@@ -0,0 +1,38 @@
+// https://leetcode.com/problems/find-common-characters/description/
+#include <iostream>
+#include <vector>
+#include <string>
+#include <limits.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<string> commonChars(vector<string> &words)
+    {
+        vector<int> common(26, INT_MAX);
+        vector<string> ans;
+
+        for (auto word : words)
+        {
+            vector<int> count(26, 0);
+            for (auto c : word)
+            {
+                count[c - 'a']++;
+            }
+            for (int i = 0; i < 26; i++)
+            {
+                common[i] = min(common[i], count[i]);
+            }
+        }
+        for (int i = 0; i < 26; i++)
+        {
+            int ch = common[i];
+            while (ch--)
+            {
+                ans.push_back(string(1, 'a' + i));
+            }
+        }
+        return ans;
+    }
+};

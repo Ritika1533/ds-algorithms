e9796c8c64fc2f2ff9d5341154a5f7ba7a80bad7
unknown <ritikakes33@gmail.com>
2024-06-16 16:16:14 +0530

    dsa

---
+++ List/01_Linked List/palindrome.cpp
@@ -0,0 +1,54 @@
+// https://leetcode.com/problems/palindrome-linked-list/description/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+    ListNode *reverseList(ListNode *head)
+    {
+        ListNode *temp = head;
+        ListNode *prev = NULL;
+        while (temp != NULL)
+        {
+            ListNode *front = temp->next;
+            temp->next = prev;
+            prev = temp;
+            temp = front;
+        }
+        return prev;
+    }
+
+public:
+    bool isPalindrome(ListNode *head)
+    {
+        ListNode *slow = head;
+        ListNode *fast = head;
+        while (fast->next != NULL && fast->next->next != NULL)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode *newHead = reverseList(slow->next);
+        ListNode *first = head;
+        ListNode *second = newHead;
+        while (second != NULL)
+        {
+            if (second->val != first->val)
+            {
+                reverseList(newHead);
+                return false;
+            }
+            first = first->next;
+            second = second->next;
+        }
+        reverseList(newHead);
+        return true;
+    }
+};
